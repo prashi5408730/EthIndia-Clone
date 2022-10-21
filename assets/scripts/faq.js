c05c3cb51a08ea7c93f968c92df18b41aa741827
Prashansa <prashansatiwari635@gmail.com>
2022-10-21 19:50:15 +0530

    today's all content committed

---
+++ assets/scripts/faq.js
@@ -0,0 +1,5 @@
+let expandBtn= Array.from(document.getElementsByClassName("expand-button"));
+let collapseBtn= Array.from(document.getElementsByClassName("collapse-button"));
+
+console.log(expandBtn);
+console.log(collapseBtn)
