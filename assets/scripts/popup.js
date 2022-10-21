0d1692372f8f9594367985540d643ab274e9e609
Prashansa <prashansatiwari635@gmail.com>
2022-10-21 19:49:26 +0530

    home, celebrating, partners, faq completed for half-screen vies

---
+++ assets/scripts/popup.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    var x = document.getElementById("popup");
+    x.style.display = "block";
+}
+
+function closePopup() {
+    var x = document.getElementById("popup");
+    x.style.display = "none";
+}
